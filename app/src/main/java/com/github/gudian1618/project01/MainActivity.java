a64829205369bae7ac514ad5674e1b7cd96c585e
gudian1618 <1761844211@qq.com>
2020-08-17 21:34:53 +0800

    new

--- app/src/main/java/com/github/gudian1618/project01/MainActivity.java
+++ app/src/main/java/com/github/gudian1618/project01/MainActivity.java
@@ -1,6 +1,9 @@
 package com.github.gudian1618.project01;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +13,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // R代表的res文件夹,R类是android自动生成的
+        // 以前java是通过文件夹来访问一个文件
+        /**
+         * android为了提高文件访问速度,把文件压缩到一个文件中了
+         * 程序变小了,访问速度也提高了
+         */
         setContentView(R.layout.activity_main);
+        // 找到textView
+        // 跟js中的document.getElementById()一样
+        TextView textView = (TextView) findViewById(R.id.login);
+        // 完成事件绑定
+        TextViewListener listener = new TextViewListener();
+        textView.setOnClickListener(listener);
+
     }
+
+    // 内部类
+    class TextViewListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            // js用alert
+            // 微信小程序用wx.showTools
+            Toast toast = Toast.makeText(MainActivity.this, "你好", Toast.LENGTH_LONG);
+            toast.show();
+        }
+    }
+
 }
